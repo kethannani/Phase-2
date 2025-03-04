820a23f4ac716d15344e945b7201a360012c4af9
kethannani <97157508+kethannani@users.noreply.github.com>
2025-03-04 13:14:55 +0530

    Create Settings.jsx

---
+++ Settings.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Settings = () => {
+  return (
+    <>
+    <div>In the Settings</div>
+    <Link to="/Contact/Settings/Ui">Go to Ui</Link>
+    </>
+  )
+}
+
+export default Settings

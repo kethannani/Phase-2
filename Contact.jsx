9c093c5b7554437b4eec2eb3400dcc596ed3f022
kethannani <97157508+kethannani@users.noreply.github.com>
2025-03-04 13:14:10 +0530

    Create Contact.jsx

---
+++ Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Contact = () => {
+  return (
+    <>
+    <div>Contact</div>
+    <Link to="/Contact/Settings">Settings</Link>
+    </>
+  )
+}
+
+export default Contact

6d1f535b1ce06381e291f82ac446c4457ad4305f
kethannani <97157508+kethannani@users.noreply.github.com>
2025-03-04 13:15:28 +0530

    Create Ui.jsx

---
+++ Ui.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Ui = () => {
+  return (
+    <>
+    <div>You are in the Ui page</div>
+    <Link to="/Contact/Settings">Settings</Link>
+    </>
+  )
+}
+
+export default Ui
